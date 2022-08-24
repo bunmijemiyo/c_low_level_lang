284c8e1c1a2f11cc90a1a116ce3e45a5bc0d4f51
olubunmi <bjemiyor@gmail.com>
2022-08-24 08:18:43 +0000

    reading from a text file

---
+++ files/write_read.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main()
+{
+    int fd;
+    char buf[14];
+
+    /* write */
+
+    fd = open("myfile.txt", O_CREAT | O_WRONLY, 0600);
+
+    if (fd == -1)
+    {
+        printf("Failed to create and open the file.\n");
+        exit(1);
+    }
+
+    write(fd, "Hello World!\n", 13);
+
+    /* read */
+
+    fd = open("myfile.txt", O_RDONLY);
+
+    if (fd == -1)
+    {
+        printf("Failed to create and open the file.\n");
+        exit(1);
+    }
+    read(fd, buf, 13);
+    buf[13] = '\0';
+
+    close(fd);
+
+    printf("buf: %s\n", buf);
+    return 0;
+}
