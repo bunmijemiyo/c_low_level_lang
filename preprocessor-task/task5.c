233e4a810593f20c646a29e5336caa309203d6a7
olubunmi <bjemiyor@gmail.com>
2022-08-31 13:28:36 +0000

    display warning, error and message with pragma

---
+++ preprocessor-task/task5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma GCC warning "hello"
+#pragma GCC error "what"
+#pragma message "ok"
+
+/* Using pragma to display warning, error and message.*/
+
+int main(void)
+{
+    printf("Hello World!\n");
+
+    return 0;
+}
+
