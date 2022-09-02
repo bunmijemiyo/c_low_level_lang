d6db38848b656fa3d9eff576a17d11031547c8e3
olubunmi <bjemiyor@gmail.com>
2022-09-02 08:33:05 +0000

    printing some macro constant

---
+++ macros/symbolicConstant.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+write a program to print the values of the following predefined symbolic constants
+__LINE__
+__FILE__
+__DATE__
+__TIME__
+__STDC__
+*/
+
+int main()
+{
+    printf("__LINE__ = %d\n", __LINE__);
+    printf("__FILE__ = %s\n", __FILE__);
+    printf("__DATE__ = %s\n", __DATE__);
+    printf("__TIME__ = %s\n", __TIME__);
+    printf("__STDC__ = %d\n", __STDC__);
+    return 0;
+}
