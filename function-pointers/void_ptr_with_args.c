7c92de3292c04078f7c794c13b6f450538b1598c
olubunmi <bjemiyor@gmail.com>
2022-09-08 11:45:52 +0000

    Created a function pointer directory

---
+++ function-pointers/void_ptr_with_args.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void func1(int);
+void func2(int);
+
+typedef void FuncType(int);
+
+int main()
+{
+    FuncType *func_ptr = NULL;
+
+    func_ptr = func1;
+    (*func_ptr)(100);
+
+    func_ptr = func2;
+    (*func_ptr)(200);
+    
+    return 0;
+}
+
+void func1(int testarg)
+{
+    printf("func1 got an argument of %d\n", testarg);
+}
+
+void func2(int testarg)
+{
+    printf("func2 got an argument of %d\n", testarg);
+}
+
